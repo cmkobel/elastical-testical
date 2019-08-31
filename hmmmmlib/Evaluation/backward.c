00bbce71a8790be60b6a9a56cb51a2c3f49df3bf
thornado123 <thor@chora.dk>
2019-08-31 11:58:11 +0200

    added backward

---
+++ hmmmmlib/Evaluation/backward.c
@@ -0,0 +1,9 @@
+//
+//  backward.c
+//  hmmmmlib
+//
+//  Created by Thor Jakobsen on 31/08/2019.
+//  Copyright © 2019 Thor Jakobsen. All rights reserved.
+//
+
+#include "backward.h"
---
+++ hmmmmlib/Evaluation/backward.h
@@ -0,0 +1,14 @@
+//
+//  backward.h
+//  hmmmmlib
+//
+//  Created by Thor Jakobsen on 31/08/2019.
+//  Copyright © 2019 Thor Jakobsen. All rights reserved.
+//
+
+#ifndef backward_h
+#define backward_h
+
+#include <stdio.h>
+
+#endif /* backward_h */
