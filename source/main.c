71717aa60e09543d5c9ace51242d86e04a336c47
Carl M. Kobel <kobel@pm.me>
2019-09-02 08:55:34 +0200

    arbejder i source

--- source/main.c
+++ source/main.c
@@ -2,8 +2,20 @@
 #include "hmm.c"
 
 int main() {
-    HMM * objectieMcObjectyface;
-    objectieMcObjectyface = HMMCreate(3, 3, 3.0);
+    /*HMM * objectieMcObjectyface;
+    objectieMcObjectyface = HMMCreate(3, 3, 3.0);*/
+    
+
+    int c, nl;
+    nl = 0;
+
+    
+    while ((c = getchar()) != EOF)
+        if (c == '\n')
+            ++nl;
+    printf("%d\n", nl);
+
 
     return 0;
+
 }
