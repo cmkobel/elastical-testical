26ddd6d882b9203cc7224649b42e8bcac9e0f40f
thornado123 <thor@chora.dk>
2019-09-05 20:59:34 +0200

    Gonna next step postier decoding

---
+++ hmmmmlib/Decoding/posteriorDecoding.h
@@ -0,0 +1,14 @@
+//
+//  posteriorDecoding.h
+//  hmmmmlib
+//
+//  Created by Thor Jakobsen on 05/09/2019.
+//  Copyright © 2019 Thor Jakobsen. All rights reserved.
+//
+
+#ifndef posteriorDecoding_h
+#define posteriorDecoding_h
+
+#include <stdio.h>
+
+#endif /* posteriorDecoding_h */
