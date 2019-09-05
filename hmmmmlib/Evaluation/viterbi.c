9108c7a4a385c17861ffc189c537db9d4d80f1c3
Carl M. Kobel <kobel@pm.me>
2019-09-05 16:50:17 +0200

    viterbi

--- hmmmmlib/Evaluation/viterbi.c
+++ hmmmmlib/Evaluation/viterbi.c
@@ -9,32 +9,18 @@ int main() {
     int n_obs = 3; // normal, cold, dizzy
     int n_states = 2; // healthy, fever
     double start_p_[2] = {0.6, 0.4};
+    
     double trans_p_[2][2] = {{0.7, 0.3}, {0.4, 0.6}};
+    double (*trans_p)[2] = trans_p_; // make a pointer to the first element
+    
     double emit_p_[2][3] = {{0.5, 0.4, 0.1}, {0.1, 0.3, 0.6}};
-    
-    // make compatible pointer types.
-    double * start_p = start_p_; 
-    
-    double ** trans_p = calloc(2, sizeof(double));
-    for (int i = 0; i < 2; i++) trans_p[i] = calloc(2, sizeof(double));
-
-    // copy
-    for (int i = 0; i < n_states; i++)
-        for (int j = 0; j < n_states; j++)
-            trans_p[i][j] = trans_p_[i][j];
-
-    double ** emit_p = calloc(2, sizeof(double));
-    for (int i = 0; i < 2; i++) emit_p[i] = calloc(2, sizeof(double));
-    
-    //copy
-    for (int i = 0; i < n_states; i++)
-        for (int j = 0; j < n_obs; j++)
-            emit_p[i][j] = emit_p_[i][j];
-    
-    
+    double (*emit_p)[3] = emit_p_; // make a pointer to the first element
 
 
-    viterbi(n_obs, n_states, *start_p, *trans_p, *emit_p);
+    // printf("%f\n", emit_p[1][1]);
+
+
+    viterbi(n_obs, n_states, *start_p_, *trans_p, *emit_p);
 
 
 
@@ -47,7 +33,9 @@ int viterbi(int n_obs, int n_states, double start_p, double *trans_p, double *em
     double rv;
     rv = 0.0;
 
-    printf("day 1:\n%f", trans_p[1][0]);
+    int observations[] = {0, 1 ,2}; // normal, cold, dizzy
+    int n_observations = 3;
+
 
     return rv;
 
