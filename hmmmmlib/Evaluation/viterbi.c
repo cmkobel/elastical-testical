12602d97a61a396c40c56ec52c20847205d8d803
Carl M. Kobel <kobel@pm.me>
2019-09-08 23:44:21 +0200

    første løkke

--- hmmmmlib/Evaluation/viterbi.c
+++ hmmmmlib/Evaluation/viterbi.c
@@ -2,47 +2,101 @@
 #include <stdlib.h>
 
 
-int viterbi(int n_obs, int n_states, double *start_p, double **trans_p, double **emit_p);
+int viterbi(int n_obs, int n_states, float *start_p, float **trans_p, float **emit_p, int n_data, int *data[]);
+
 
 int main() {
     // set hardcoded probs
-    int n_obs = 3; // normal, cold, dizzy
     int n_states = 2; // healthy, fever
-    double start_p_[2] = {0.6, 0.4};
-    double trans_p_[2][2] = {{0.7, 0.3}, {0.4, 0.6}};
-    double emit_p_[2][3] = {{0.5, 0.4, 0.1}, {0.1, 0.3, 0.6}};
-    
-    // make compatible pointer types.
-    double * start_p = start_p_; 
-    
-    double ** trans_p = calloc(2, sizeof(double *));
-    //for(int i = 0; i < 2; i++) trans_p[i] = calloc(2, sizeof(double));
-    for (int i = 0; i < 2; i++) trans_p[i] = trans_p_[i];
+    int n_obs = 3; // normal, cold, dizzy
 
-    double ** emit_p = calloc(2, sizeof(double *));
-    //for (int i= 0; i < 2; i++) emit_p[i] = calloc(3, sizeof(double));
+    float start_p_[2] = {0.6, 0.4};
+    float* start_p = start_p_; // Hvilken forskel gør den her linje? Måske kan jeg nøjes med et &-tegn ved viterbi(), og udelade denne linje?
+
+    float trans_p_[2][2] = {{0.7, 0.3}, {0.4, 0.6}};
+    float** trans_p = calloc(2, sizeof(float*));
+    for (int i = 0; i < 2; i++) trans_p[i] = trans_p_[i]; // Hvornår allokerer vi kolonnerne?
+
+    float emit_p_[2][3] = {{0.5, 0.4, 0.1}, {0.1, 0.3, 0.6}}; // [state][obs]
+    float** emit_p = calloc(2, sizeof(float*));
     for (int i = 0; i < 2; i++) emit_p[i] = emit_p_[i];
-
+    
     
 
 
-    viterbi(n_obs, n_states, start_p, trans_p, emit_p);
+
+    
+    // feed data to the viterbi alg.
+
+    int n_data = 4; // normal, cold, dizzy, dizzy
+    int data_[4] = {0, 1, 2, 2}; // normal, cold, dizzy, dizzy
+    int *data = data_;
+    
+
+
+    viterbi(n_obs, n_states, start_p, trans_p, emit_p, n_data, &data);
+
 
 
 
     return 0;
 }
+ 
 
+int viterbi(int n_states, int n_obs, float *start_p, float **trans_p, float **emit_p, int n_data, int *data[]) {
 
-int viterbi(int n_obs, int n_states, double *start_p, double **trans_p, double **emit_p) {
-
-    double rv;
-    rv = 0.0;
     
-    for (int i = 0; i < 2; i++) 
-        printf("%f %f\n", trans_p[0+i][0], trans_p[0+i][1]);
+    //initialiser returnerings-arrays
+    //float output[] = calloc(n_states, sizeof(float*));
 
-    return rv;
+    float* probs = calloc(n_states, sizeof(float)); 
+
+    int hidden_state = -1; // -1 for uninitialized value
+
+    // set initial emission probs
+    for (int i = 0; i < n_data; ++i) {
+        if (hidden_state == -1) {
+            // first iteration. Compute only with start_p
+            for (int j = 0; j < n_states-1; j++) {
+                probs[j] = start_p[j]*emit_p[j][i];
+                printf("%d>%f ", j, probs[j]);
+
+            }
+            printf("\n");
+        }
+        
+        else {
+            // All iterations after the first.
+            ;
+
+        
+
+
+
+            
+        }
+
+        // Find the hidden state
+        float max_ = 0.0;
+        for (int j = 0; j < n_states-1; j++) {
+                if (probs[j] > max_) {
+                    max_ = probs[j];
+                    hidden_state = j;
+                }
+            }
+        printf("hs is %d with p = %f\n\n", hidden_state, max_);
+        
+        break;
+
+
+
+        
+        
+    }
+    
+
+    
+    return 0;
 
 
 }
