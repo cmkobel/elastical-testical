4d20c83082b90506374ddc277f1b8e81250b149c
thornado123 <thor@chora.dk>
2019-09-09 22:07:55 +0200

    all up and working with cmake! DO NOT CHANGE STRUCTURE

---
+++ hmmmmlib/build/CMakeFiles/HMMLIB.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/src/Decoding/posteriorDecoding.c" "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/build/CMakeFiles/HMMLIB.dir/src/Decoding/posteriorDecoding.c.o"
+  "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/src/Decoding/viterbi.c" "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/build/CMakeFiles/HMMLIB.dir/src/Decoding/viterbi.c.o"
+  "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/src/Evaluation/backward.c" "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/build/CMakeFiles/HMMLIB.dir/src/Evaluation/backward.c.o"
+  "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/src/Evaluation/forward.c" "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/build/CMakeFiles/HMMLIB.dir/src/Evaluation/forward.c.o"
+  "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/src/HMM/hmm.c" "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/build/CMakeFiles/HMMLIB.dir/src/HMM/hmm.c.o"
+  "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/src/Test/TestBaumWelch.c" "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/build/CMakeFiles/HMMLIB.dir/src/Test/TestBaumWelch.c.o"
+  "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/src/Test/TestViterbi.c" "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/build/CMakeFiles/HMMLIB.dir/src/Test/TestViterbi.c.o"
+  "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/src/Test/TestsForwardAlgorithm.c" "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/build/CMakeFiles/HMMLIB.dir/src/Test/TestsForwardAlgorithm.c.o"
+  "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/src/Test/testBackwardAlgorithm.c" "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/build/CMakeFiles/HMMLIB.dir/src/Test/testBackwardAlgorithm.c.o"
+  "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/src/Training/baumWelch.c" "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/build/CMakeFiles/HMMLIB.dir/src/Training/baumWelch.c.o"
+  "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/src/main.c" "/Users/thorjakobsen/GIT/elastical-testical/hmmmmlib/build/CMakeFiles/HMMLIB.dir/src/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../src"
+  "../src/Decoding"
+  "../src/Evaluation"
+  "../src/HMM"
+  "../src/Test"
+  "../src/Training"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ hmmmmlib/build/CMakeFiles/HMMLIB.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HMMLIB.dir/src/Decoding/posteriorDecoding.c.o"
+  "CMakeFiles/HMMLIB.dir/src/Decoding/viterbi.c.o"
+  "CMakeFiles/HMMLIB.dir/src/Evaluation/backward.c.o"
+  "CMakeFiles/HMMLIB.dir/src/Evaluation/forward.c.o"
+  "CMakeFiles/HMMLIB.dir/src/HMM/hmm.c.o"
+  "CMakeFiles/HMMLIB.dir/src/Test/TestBaumWelch.c.o"
+  "CMakeFiles/HMMLIB.dir/src/Test/TestViterbi.c.o"
+  "CMakeFiles/HMMLIB.dir/src/Test/TestsForwardAlgorithm.c.o"
+  "CMakeFiles/HMMLIB.dir/src/Test/testBackwardAlgorithm.c.o"
+  "CMakeFiles/HMMLIB.dir/src/Training/baumWelch.c.o"
+  "CMakeFiles/HMMLIB.dir/src/main.c.o"
+  "HMMLIB"
+  "HMMLIB.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/HMMLIB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
