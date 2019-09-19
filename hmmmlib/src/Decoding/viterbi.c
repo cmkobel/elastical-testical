8b997fbe9226eba29896ba4965d063a1826dbd19
Carl M. Kobel <kobel@pm.me>
2019-09-19 17:05:54 +0200

    linear array-ificize

--- hmmmlib/src/Decoding/viterbi.c
+++ hmmmlib/src/Decoding/viterbi.c
@@ -3,23 +3,9 @@
 #include <stdlib.h>
 #include "viterbi.h"
 
-unsigned int arrayMax(double* array, unsigned int n) {
-    // Returns the index of the maximum element.
-    // Would it be faster to use more pointers?  
-    double maximum = -INFINITY;
-    unsigned int max_index = 0;
-    for (unsigned int i = 0; i < n; i++) {
-        if (array[i] > maximum) {
-            maximum = array[i];
-            max_index = i;
-        }
-    }
-    return max_index;
-}
 
-unsigned int* viterbi(HMM *hmm, const int *Y, const int T) {
+unsigned int* viterbi(HMM *hmm, const unsigned int *Y, const unsigned int T) {
     // Y is the data, T is the length of the data
-
     
     
     unsigned int i;
@@ -27,15 +13,12 @@ unsigned int* viterbi(HMM *hmm, const int *Y, const int T) {
     unsigned int j;
     
     // allocate matrix[data][states]
-    double** table = calloc(T, sizeof(double*)); //
-    for (i = 0; i < T; ++i) {
-        table[i] = calloc(hmm->hiddenStates, sizeof(double));
-    }
-    
+    double* table = calloc(T*hmm->hiddenStates, sizeof(double*)); //
 
+    
     // Compute initial probabilities
     for (i = 0; i < hmm->hiddenStates; ++i) {
-        table[0][i] = log(hmm->initProbs[i]) + log(hmm->emissionProbs[i*hmm->observations+Y[0]]);
+        table[0*hmm->hiddenStates+i] = log(hmm->initProbs[i]) + log(hmm->emissionProbs[i*hmm->observations+Y[0]]);
     }
 
     
@@ -44,41 +27,54 @@ unsigned int* viterbi(HMM *hmm, const int *Y, const int T) {
         for (k = 0; k < hmm->hiddenStates; ++k) {
             double value = -INFINITY;
             for (j = 0; j < hmm->hiddenStates; ++j) {
-                if (table[i-1][j] + log(hmm->transitionProbs[j*hmm->hiddenStates+k]) > value) {
-                    value = table[i-1][j] + log(hmm->transitionProbs[j*hmm->hiddenStates+k]);
+                if (table[(i-1)*hmm->hiddenStates+j] + log(hmm->transitionProbs[j*hmm->hiddenStates+k]) > value) {
+                    value = table[(i-1)*hmm->hiddenStates+j] + log(hmm->transitionProbs[j*hmm->hiddenStates+k]);
                 }
             }
-            table[i][k] = log(hmm->emissionProbs[k*hmm->observations+Y[i]]) + value;
+            table[i*hmm->hiddenStates+k] = log(hmm->emissionProbs[k*hmm->observations+Y[i]]) + value;
         }
     }
 
-    /* 
+    
     // debug print table
-    if (false) { 
-        printf("\n");
-        for (int row = 0; row < T; row++) {
-            for (int col = 0; col < hmm->hiddenStates; col++) {
-                printf("%9.3f  ", table[row][col]);
-            }
-            printf("\n");
+    /* 
+    printf("\n");
+    for (int row = 0; row < T; row++) {
+        for (int col = 0; col < hmm->hiddenStates; col++) {
+            printf("%9.3f  ", table[row*hmm->hiddenStates + col]);
         }
         printf("\n");
-    }    
+    }
+    printf("\n");
      */
-    
+   
     // Backtrack
     unsigned int* decodedStates = calloc(T, sizeof(int));
-    decodedStates[T-1] = arrayMax(table[T-1], hmm->hiddenStates); // havde jeg ikke en minus 1 for meget her?
+
+    // Find max value in last row in table[]
+    unsigned int temp_max_index_ = 0;
+    double temp_maximum = -INFINITY;
+    for (unsigned int m = 0; m < hmm->hiddenStates; m++) {
+        if (table[((T-1) * hmm->hiddenStates )+ m] > temp_maximum) {
+            temp_max_index_ = m;
+            temp_maximum = table[(T-1) * hmm->hiddenStates + m];
+            //printf("max is %f\n", temp_maximum);
+        }
+    }
+    decodedStates[T-1] = temp_max_index_;
+    
+    // finish backtracking
     for (unsigned int i = T-1; i > 0; i--) {
         for (unsigned int j = 0; j < hmm->hiddenStates; j++) {            
-            if (table[i-1][j] + log(hmm->transitionProbs[j*hmm->hiddenStates + decodedStates[i]]) + log(hmm->emissionProbs[decodedStates[i]*hmm->observations + Y[i]]) == table[i][decodedStates[i]]) {
+            if (table[(i-1)*hmm->hiddenStates + j] + log(hmm->transitionProbs[j*hmm->hiddenStates + decodedStates[i]]) + log(hmm->emissionProbs[decodedStates[i]*hmm->observations + Y[i]]) == table[i*hmm->hiddenStates + decodedStates[i]]) {
                 decodedStates[i-1] = j;
                 break;
             }
         }
         
     }
-    
+
+
 
     return decodedStates;
 
--- hmmmlib/src/Decoding/viterbi.h
+++ hmmmlib/src/Decoding/viterbi.h
@@ -1,5 +1,5 @@
 #include "hmm.h"
 
-unsigned int* viterbi(HMM *hmm, const int *Y, const int T);
+unsigned int* viterbi(HMM *hmm, const unsigned int *Y, const unsigned int T);
 
-unsigned int arrayMax(double* array, unsigned int length_of_array);
+
