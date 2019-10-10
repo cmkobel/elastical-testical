b6229ae34309c78856f666974611bee6fc4f9b7e
Carl M. Kobel <kobel@pm.me>
2019-10-10 18:51:16 +0200

    plot

--- test_framework/plots/render_plots.R
+++ test_framework/plots/render_plots.R
@@ -1,13 +1,24 @@
 library(tidyverse)
+library(ggpubr)
 
-viterbi_normal <- read_csv("viterbi_normal.csv",)
+viterbi_normal <- read_csv("bioinformatics/hmm/hmmmlib/test_framework/plots/viterbi_normal.csv",)
 
 viterbi_normal_processed = viterbi_normal %>% group_by(observations) %>% 
     summarise(mean = mean(time), sd = sd(time)) 
 
-viterbi_normal_processed %>% ggplot(aes(observations, mean)) +
-    geom_line() + 
+viterbi_plot_1 = viterbi_normal_processed %>% ggplot(aes(observations, mean)) +
+    geom_point() + 
     geom_errorbar(aes(ymin=mean-sd, ymax=mean+sd)) +
     labs(y = 'mean time')
 
 
+viterbi_plot_2 = viterbi_normal_processed %>% ggplot(aes(observations, mean/observations)) +
+    geom_line() + 
+    geom_errorbar(aes(ymin=mean/observations-sd, ymax=mean/observations+sd)) +
+    labs(y = 'mean time normalized')
+
+
+
+ggarrange(viterbi_plot_1, viterbi_plot_2, ncol = 1, nrow = 2, align = "v")
+
+ggsave("viterbi.png", height = 4, width = 8)
--- test_framework/running_time.py
+++ test_framework/running_time.py
@@ -1,7 +1,7 @@
 import binding as hmm_binding
 import time
 
-def read_fasta(n_lines, file = 'data/pantro3_X.fasta'):
+def read_fasta(n_lines, file):
     mapping = {letter: index for index, letter in enumerate(['A', 'C', 'G', 'T'])}
     
     with open(file, 'r') as fasta_file:
@@ -14,43 +14,41 @@ def read_fasta(n_lines, file = 'data/pantro3_X.fasta'):
                 yield number
 
 
-#print([i for i in read_fasta(10)])
+
+def running_time_viterbi(start = 1000, stop = 100000, increment = 5000, replicates = 5, file = 'data/pantro3_X.fasta', linewidth = 60):
+    ## Test viterbi setup ##
+    o = hmm_binding.binded_HMM(7, 4)
+    o.setInitProbs([0.00, 0.00, 0.00, 1.00, 0.00, 0.00, 0.00])
+    o.setTransitionProbs([[0.00, 0.00, 0.90, 0.10, 0.00, 0.00, 0.00],
+                        [1.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
+                        [0.00, 1.00, 0.00, 0.00, 0.00, 0.00, 0.00],
+                        [0.00, 0.00, 0.05, 0.90, 0.05, 0.00, 0.00],
+                        [0.00, 0.00, 0.00, 0.00, 0.00, 1.00, 0.00],
+                        [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 1.00],
+                        [0.00, 0.00, 0.00, 0.10, 0.90, 0.00, 0.00]])
+    o.setEmissionProbs([[0.30, 0.25, 0.25, 0.20],
+                        [0.20, 0.35, 0.15, 0.30],
+                        [0.40, 0.15, 0.20, 0.25],
+                        [0.25, 0.25, 0.25, 0.25],
+                        [0.20, 0.40, 0.30, 0.10],
+                        [0.30, 0.20, 0.30, 0.20],
+                        [0.15, 0.30, 0.20, 0.35]])
 
 
-## Test viterbi setup ##
-
-o = hmm_binding.binded_HMM(7, 4)
-o.setInitProbs([0.00, 0.00, 0.00, 1.00, 0.00, 0.00, 0.00])
-o.setTransitionProbs([[0.00, 0.00, 0.90, 0.10, 0.00, 0.00, 0.00],
-                      [1.00, 0.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                      [0.00, 1.00, 0.00, 0.00, 0.00, 0.00, 0.00],
-                      [0.00, 0.00, 0.05, 0.90, 0.05, 0.00, 0.00],
-                      [0.00, 0.00, 0.00, 0.00, 0.00, 1.00, 0.00],
-                      [0.00, 0.00, 0.00, 0.00, 0.00, 0.00, 1.00],
-                      [0.00, 0.00, 0.00, 0.10, 0.90, 0.00, 0.00]])
-o.setEmissionProbs([[0.30, 0.25, 0.25, 0.20],
-                    [0.20, 0.35, 0.15, 0.30],
-                    [0.40, 0.15, 0.20, 0.25],
-                    [0.25, 0.25, 0.25, 0.25],
-                    [0.20, 0.40, 0.30, 0.10],
-                    [0.30, 0.20, 0.30, 0.20],
-                    [0.15, 0.30, 0.20, 0.35]])
-
-
-## Test viterbi loop ##
-
-
-start, stop, increment = 1000, 100000, 5000
-linewidth = 60
-replicates = 5
-print('observations, time')
-for i in range(start, stop, increment):
-    test_viterbi_data = [i for i in read_fasta(i)]
-    for replicate in range(replicates):
+    ## Test viterbi loop ##
+    print('observations, time')
+    for i in range(start, stop, increment):
+        test_viterbi_data = [i for i in read_fasta(i, file)]
+        for replicate in range(replicates):
+            
+            t0 = time.time()
+            test_viterbi_output = o.viterbi(test_viterbi_data)
+            t1 = time.time()
+            print(f'{i*linewidth}, {t1-t0}')
         
-        t0 = time.time()
-        test_viterbi_output = o.viterbi(test_viterbi_data)
-        t1 = time.time()
-        print(f'{i*linewidth}, {t1-t0}')
-    
-    #print(test_viterbi_output[:100])
+        #print(test_viterbi_output[:100])
+
+
+running_time_viterbi(1000000, 2000000, 1000000)
+
+
