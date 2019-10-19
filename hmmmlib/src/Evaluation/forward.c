d611f4354b9ae5fdda11c7b0a9af6ef82917ecfe
thornado123 <thor@chora.dk>
2019-10-19 17:12:27 +0200

    forward working... have to restructure alpha

--- hmmmlib/src/Evaluation/forward.c
+++ hmmmlib/src/Evaluation/forward.c
@@ -17,69 +17,26 @@ double * forward(HMM *hmm, const int *Y, const int T, double * scalingFactor){
         new_emission_probs[i] = matrix;
     }
     
-    double * result = calloc(hmm->hiddenStates, sizeof(double));
-    // Doing the matrix multiplication
-    cblas_dsymv(CblasRowMajor, 121, hmm->hiddenStates, 1.0, new_emission_probs[Y[0]], hmm->hiddenStates, hmm->initProbs, 1, 1, result, 1);
-    // Scaling
-    cblas_dscal(hmm->hiddenStates, (1.0/cblas_dasum(hmm->hiddenStates, result, 1)), result, 1);
+    double * alpha = malloc(hmm->hiddenStates*T*sizeof(double));
     
-    for(i = 0; i < hmm->hiddenStates; i++){
-        printf("%f, ", result[i]);
+    // Doing the matrix multiplication and then scalingFactor
+    cblas_dsymv(CblasRowMajor, 121, hmm->hiddenStates, 1.0, new_emission_probs[Y[0]], hmm->hiddenStates, hmm->initProbs, 1, 1, alpha, 1);
+    scalingFactor[0] = cblas_dasum(hmm->hiddenStates, alpha, 1);
+    cblas_dscal(hmm->hiddenStates, (1.0/scalingFactor[0]), alpha, 1);
+    
+    for(i = 1; i<T; i++){
+        cblas_dgemv(CblasRowMajor, CblasTrans, hmm->hiddenStates, hmm->hiddenStates, 1.0, hmm->transitionProbs, hmm->hiddenStates, alpha+2*(i-1), 1, 0, alpha+2*i, 1);
+        cblas_dgemv(CblasRowMajor, CblasTrans, hmm->hiddenStates, hmm->hiddenStates, 1.0, new_emission_probs[Y[i]], hmm->hiddenStates, alpha+2*i, 1, 0, alpha+2*i, 1);
+        scalingFactor[i] = cblas_dasum(hmm->hiddenStates, alpha+2*i, 1);
+        cblas_dscal(hmm->hiddenStates, (1.0/scalingFactor[i]), alpha+2*i, 1);
     }
     
-    cblas_dgemv(CblasRowMajor, CblasTrans, hmm->hiddenStates, hmm->hiddenStates, 1.0, hmm->transitionProbs, hmm->hiddenStates, result, 1, 0, result, 1);
-    cblas_dgemv(CblasRowMajor, CblasTrans, hmm->hiddenStates, hmm->hiddenStates, 1.0, new_emission_probs[Y[1]], hmm->hiddenStates, result, 1, 0, result, 1);
-    cblas_dscal(hmm->hiddenStates, (1.0/cblas_dasum(hmm->hiddenStates, result, 1)), result, 1);
-    
-    for(i = 0; i < hmm->hiddenStates; i++){
-        printf("%f, ", result[i]);
-    }
-    
-
-    // 2D alpha matrix
-    //
-    // [state][time]
-    //
-    double * alpha = calloc(hmm->hiddenStates*T, sizeof(double));
-
-    // Initial is the same as the initProbs times the probs of emitting Y[0]
-    for(i = 0; i < hmm->hiddenStates; i++){
-        alpha[i*T] = hmm->initProbs[i]*hmm->emissionProbs[i*hmm->observations+Y[0]];
-        scalingFactor[0] += alpha[i*T+0];
-    }
-    
-    // Scaling step
-    for(j = 0; j < hmm->hiddenStates; j++){
-        alpha[j*T+0] = alpha[j*T+0]/scalingFactor[0];
-    }
-
-    // Now the "recursive" step starts
-    for(i = 1; i < T; i++){
+    for(i = 0; i < T; i++){
         for(j = 0; j < hmm->hiddenStates; j++){
-            double emissionProb = hmm->emissionProbs[j*hmm->observations+Y[i]];
-            if(emissionProb > 0){
-                double pastTransProb = 0.0;
-                for(int l = 0; l < hmm->hiddenStates; l++){
-                    pastTransProb += hmm->transitionProbs[l*hmm->hiddenStates+j]*alpha[l*T+i-1];
-                }
-                alpha[j*T+i] = emissionProb*pastTransProb;
-            }
-            scalingFactor[i] += alpha[j*T+i];
-        }
-        // Scaling step
-        for(j = 0; j < hmm->hiddenStates; j++){
-            alpha[j*T+i] = alpha[j*T+i]/scalingFactor[i];
-        }
-    }
-    
-    printf("\n\n");
-    for(i = 0; i < hmm->hiddenStates; i++) {
-        for (j = 0; j < T; j++){
-            printf("%f, ", alpha[i*T+j]);
+            printf("%f, ", alpha[i*hmm->hiddenStates+j]);
         }
         printf("\n");
     }
-    printf("\n");
-    
+
     return alpha;
 }
