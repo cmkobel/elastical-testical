f8263c257c33221a624a27dfa30db7aee7002832
Carl M. Kobel <kobel@pm.me>
2019-10-31 08:35:44 +0100

    posterior travis

---
+++ hmmmlib/Test/TestPosteriorDecoding.h
@@ -0,0 +1,17 @@
+//
+//  TestPosteriorDecoding.h
+//  hmmmmlib
+//
+//  Created by Thor Jakobsen on 09/09/2019.
+//  Copyright © 2019 Thor Jakobsen. All rights reserved.
+//
+
+#ifndef TestPosteriorDecoding_h
+#define TestPosteriorDecoding_h
+
+#include <stdbool.h>
+#include <stdio.h>
+
+extern bool testPosteriorDecoding(void);
+
+#endif
