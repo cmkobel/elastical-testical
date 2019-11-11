f7c64fe3f9ff54fb2768c40b5e9758818dac7c70
thornado123 <thor@chora.dk>
2019-11-11 21:46:04 +0100

    Needlinking help

--- hmmmlib/src/Evaluation/blas/forward_blas.c
+++ hmmmlib/src/Evaluation/blas/forward_blas.c
@@ -1,7 +1,7 @@
 #include "forward_blas.h"
 #include <stdlib.h>
-#include <Accelerate/Accelerate.h> // for mac os
-//#include <cblas.h> // for GNUlinux
+//#include <Accelerate/Accelerate.h> // for mac os
+#include <cblas.h> // for GNUlinux
 
 void forward_blas(HMM *hmm, const unsigned int *Y, const unsigned int T, double * scalingFactor, double * alpha){
     
--- hmmmlib/src/Evaluation/sblas/forward_sblas.c
+++ hmmmlib/src/Evaluation/sblas/forward_sblas.c
@@ -1,7 +1,8 @@
 #include "forward_sblas.h"
 #include <stdlib.h>
-#include <Accelerate/Accelerate.h> // for mac os
-//#include <cblas.h> // for GNUlinux
+#include <rsb.h>
+//#include <Accelerate/Accelerate.h> // for mac os
+#include <cblas.h> // for GNUlinux
 
 int hulla_csr(HMM * hmm, double ** sparseMatrixs, int * ia, int * ja, double * a){
     
@@ -31,7 +32,7 @@ int hulla_csr(HMM * hmm, double ** sparseMatrixs, int * ia, int * ja, double * a
 }
 
 void forward_sblas(HMM *hmm, const unsigned int *Y, const unsigned int T, double * scalingFactor, double * alpha){
-
+    
     unsigned int i;
     unsigned int j;
     
@@ -97,6 +98,95 @@ void forward_sblas(HMM *hmm, const unsigned int *Y, const unsigned int T, double
             printf("%f, ",a[i*znn+j]);
         }
         printf("\n");
+    }
+
+        struct rsb_mtx_t *mtxAp = NULL; /* matrix structure pointer */
+    const int bs = RSB_DEFAULT_BLOCKING;
+    const int brA = bs, bcA = bs;
+    const RSB_DEFAULT_TYPE one = 1;
+    rsb_type_t typecode = RSB_NUMERICAL_TYPE_DEFAULT;
+    rsb_err_t errval = RSB_ERR_NO_ERROR;
+    const rsb_nnz_idx_t nnzA = 4;       /* matrix nonzeroes count */
+    const rsb_coo_idx_t nrA = 3;        /* matrix rows count */
+    const rsb_coo_idx_t ncA = 3;        /* matrix columns count */
+    /* nonzero row indices coordinates: */
+    rsb_coo_idx_t IA[] = {0,1,1,2,2};
+    /* nonzero column indices coordinates: */
+    rsb_coo_idx_t JA[] = {0,1,2,2,2};
+    RSB_DEFAULT_TYPE VA[] = {11,10,22,32,1};/* values of nonzeroes */
+    RSB_DEFAULT_TYPE X[] = { 0, 0, 0 }; /* X vector's array */
+    const RSB_DEFAULT_TYPE B[] = { -1, -2, -5 }; /* B vector's array */
+    char ib[200];
+
+    printf("Hello, RSB!\n");
+    printf("Initializing the library...\n");
+    if((errval = rsb_lib_init(RSB_NULL_INIT_OPTIONS)) != 
+            RSB_ERR_NO_ERROR)
+    {
+        printf("Error initializing the library!\n");
+    }
+    printf("Correctly initialized the library.\n");
+
+    printf("Attempting to set the"
+           " RSB_IO_WANT_EXTRA_VERBOSE_INTERFACE library option.\n");
+    {
+        rsb_int_t evi=1; 
+        /* Setting a single optional library parameter. */
+        errval = rsb_lib_set_opt(
+            RSB_IO_WANT_EXTRA_VERBOSE_INTERFACE, &evi);
+        if(errval != RSB_ERR_NO_ERROR)
+        {
+            char errbuf[256];
+            rsb_strerror_r(errval,&errbuf[0],sizeof(errbuf));
+            printf("Failed setting the"
+            " RSB_IO_WANT_EXTRA_VERBOSE_INTERFACE"
+            " library option (reason string:\n%s).\n",errbuf);
+            if(errval&RSB_ERRS_UNSUPPORTED_FEATURES)
+            {
+              printf("This error may be safely ignored.\n");
+            }
+            else
+            {
+              printf("Some unexpected error occurred!\n");
+            }
+        }
+        else
+        {
+            printf("Setting back the "
+                "RSB_IO_WANT_EXTRA_VERBOSE_INTERFACE"
+                " library option.\n");
+            evi = 0;
+            errval = rsb_lib_set_opt(RSB_IO_WANT_EXTRA_VERBOSE_INTERFACE,
+                    &evi);
+            errval = RSB_ERR_NO_ERROR;
+        }
+    }
+
+    mtxAp = rsb_mtx_alloc_from_coo_const(
+        VA,IA,JA,nnzA,typecode,nrA,ncA,brA,bcA,
+        RSB_FLAG_NOFLAGS    /* default format will be chosen */
+        |RSB_FLAG_DUPLICATES_SUM/* duplicates will be summed */
+            ,&errval);
+    if((!mtxAp) || (errval != RSB_ERR_NO_ERROR))
+    {
+        printf("Error while allocating the matrix!\n");
+    }
+    printf("Correctly allocated a matrix.\n");
+    printf("Summary information of the matrix:\n");
+    /* print out the matrix summary information  */
+    rsb_mtx_get_info_str(mtxAp,"RSB_MIF_MATRIX_INFO__TO__CHAR_P",
+            ib,sizeof(ib));
+    printf("%s",ib);
+    printf("\n");
+
+    if((errval = 
+        rsb_spmv(RSB_TRANSPOSITION_N,&one,mtxAp,B,1,&one,X,1))
+            != RSB_ERR_NO_ERROR )
+    {
+        printf("Error performing a multiplication!\n");
+    }
+    for(int i = 0; i < 3; i++){
+        printf("%f \n", X[i]);
     }
     
     for(i = 1; i<T; i++){
